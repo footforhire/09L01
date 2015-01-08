0d6d67ffd52c3a35f684dab6a1e0274dc87518aa
Mark Nitz <marknitz@gmail.com>
2015-01-08 15:59:44 +0200

    Initial Commit
    Initial Commit

---
+++ main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  09L01
+//
+//  Created by Mark Nitz on 1/8/15.
+//  Copyright (c) 2015 Mark Nitz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    signed char ch;
+    int x;
+    unsigned int y;
+    
+    ch=0xFF;
+    x=0xFFFF;
+    y=0xFFFFu;
+    
+    printf("The decimal of signed 0xFF is %d.\n",ch);
+    printf("The decimal of signed 0xFFFF is %d.\n",x);
+    printf("The decimal of unsigned 0xFFFFu is %u.\n",y);
+    
+    printf("The hex of decimal 12345 is 0x%X.\n",12345);
+    printf("The hex of decimal -12345 is 0x%X.\n", -12345);
+    return 0;
+}
